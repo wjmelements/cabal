8f8e90439239a3ef28affa8375cc1421b2c7bc77
William Morriss <wjmelements@gmail.com>
2018-01-18 16:17:15 -0800

    VOTE token

--- contracts/prop2/allproposals.sol
+++ contracts/prop2/allproposals.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.18;
 // v2 goal: shared proposal pool
 // I can merge AllProposals and AllCabals and AllMembers because they have the same address space
 
-// TODO Dai
+// TODO Dai or VOTE
 interface ERC20 {
     function totalSupply() public constant returns (uint supply);
     function balanceOf(address _owner) public constant returns (uint balance);
@@ -13,31 +13,92 @@ interface ERC20 {
     event Transfer(address indexed _from, address indexed _to, uint _value);
     event Approval(address indexed _owner, address indexed _spender, uint _value);
 }
-contract Proposal {
-    uint256 constant argumentBounty = 1 finney;
-    uint256 constant voteBounty = 100 szabo;
+contract Vote is ERC20 {
+    uint256 supply;
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) approved;
+    mapping (address => uint256) faucetDate;
+
+    AllCabals allCabals; // FIXME initialize this
+    address public developerFund;
+
+    function Vote(address _developerFund, uint256 _fundSize) public {
+        developerFund = _developerFund;
+        supply = _fundSize;
+        balances[_developerFund] = _fundSize;
+    }
+    function totalSupply() public constant returns (uint) {
+        return supply;
+    }
+    function balanceOf(address _owner) public constant returns (uint) {
+        return balances[_owner];
+    }
+    function approve(address _spender, uint _value) public returns (bool) {
+        approved[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+    }
+    function allowance(address _owner, address _spender) public constant returns (uint) {
+        return approved[_owner][_spender];
+    }
+    function transfer(address _to, uint _value) public returns (bool) {
+        assert(balances[msg.sender] <= _value);
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        Transfer(msg.sender, _to, _value);
+    }
+    function transferFrom(address _from, address _to, uint _value) public returns (bool) {
+        assert(balances[_from] >= _value);
+        assert(approved[_from][msg.sender] >= _value);
+        approved[_from][msg.sender] -= _value;
+        balances[_from] -= _value;
+        balances[_to] += _value;
+        Transfer(_from, _to, _value);
+    }
+    function faucet() external {
+        assert(faucetDate[msg.sender] + 8 hours <= now);
+        assert(allCabals.canVote(msg.sender));
+        faucetDate[msg.sender] = now;
+        balances[msg.sender]+=2;
+        supply+=2;
+    }
+    function vote(address _voter, address _votee) public {
+        assert(allCabals.isProposal(msg.sender)); // FIXME use DELEGATECALL
+        assert(allCabals.canVote(_voter)); // TODO consider removing this check?
+        assert(balances[_voter] > 0);
+        balances[_voter] -= 2;
+        balances[developerFund]++;
+        balances[_votee]++;
+    }
+}
+interface ProposalInterface {
+    function getPosition(address user) public view returns (Proposal.Position);
+    function argumentCount() public view returns (uint256);
+    function vote(uint256 argumentId) external payable;
+    function resolution() public view returns (bytes);
+    function voteCount() public view returns (uint256);
+}
+contract Proposal is ProposalInterface {
     mapping (address => uint256) public votes;
 
-    function getPosition(address user)
+    Vote voteToken; // FIXME initialize
+
+    function getPosition(address _user)
     public view
     returns (Position) {
-        return arguments[votes[user]].position;
+        return arguments[votes[_user]].position;
     }
 
-    modifier setVote(uint256 argumentId) {
+    modifier setVote(uint256 _argumentId) {
         _;
         arguments[votes[msg.sender]].count--;
-        arguments[votes[msg.sender] = argumentId].count++;
+        arguments[votes[msg.sender] = _argumentId].count++;
     }
 
     event FailedMicropayment(address destination);
-    modifier pays(uint256 argumentId, uint256 amount) {
+    modifier pays(uint256 argumentId) {
         _;
-        require(msg.value >= amount);
         address destination = arguments[argumentId].source;
-        if (!destination.send(msg.value)) {
-            FailedMicropayment(destination);
-        }
+        voteToken.vote(msg.sender, destination);
     }
     
     enum Position {
@@ -62,9 +123,9 @@ contract Proposal {
         return arguments.length;
     }
 
-    function Proposal(bytes _resolution)
+    function Proposal(bytes _resolution, address _source)
     public {
-        arguments.push(Argument(msg.sender, Position.SKIP, 0, _resolution));
+        arguments.push(Argument(_source, Position.SKIP, 0, _resolution));
     }
 
     function resolution()
@@ -88,7 +149,7 @@ contract Proposal {
     function argue(Position position, bytes text)
     external payable
     setVote(arguments.length)
-    pays(0, argumentBounty)
+    pays(0)
     returns (uint256) {
         uint256 argumentId = arguments.length;
         arguments.push(Argument(msg.sender, position, 0, text));
@@ -98,7 +159,7 @@ contract Proposal {
     function vote(uint256 argumentId)
     external payable
     setVote(argumentId)
-    pays(argumentId, voteBounty) {
+    pays(argumentId) {
     }
 }
 contract Cabal {
@@ -316,7 +377,7 @@ contract Cabal {
     external
     mustBe(Membership.MEMBER) {
         require(membership[proposal] == Membership.UNCONTACTED);
-        require(proposal.getPosition(msg.sender) == Proposal.Position.APPROVE); // FIXME use some other way to verify it is a proposal? 
+        require(proposal.getPosition(msg.sender) == Proposal.Position.APPROVE); // FIXME use AllProposals to verify it is a proposal
         membership[proposal] = Membership.PROPOSAL;
         proposals.push(proposal);
         NewProposal(proposal);
@@ -347,34 +408,152 @@ contract Cabal {
     }
 }
 contract AllCabals {
+    
+    uint256 constant public registrationBounty = 1 finney;
+    uint256 constant public outsideProposalVerificationFee = 50 finney;
+    uint256 constant public outsideProposalRejectionBurn = 25 finney;
+    uint256 constant public outsideProposalRejectionBounty = 25 finney;
+
     enum Membership {
         // default
         UNCONTACTED,
-        //
-        PENDING,
-        //
-        CABAL
+        VOTER,
+        PENDING_PROPOSAL,
+        PROPOSAL,
+        PENDING_CABAL,
+        CABAL,
+        BOARD
+        // TODO determine if proposals and cabals can be board members
+        // TODO if proposals can be board members, we have to update the vote token code
     }
-    struct CabalInfo {
+    struct Info {
         Membership membership;
-        bytes data;
+        uint256 registrationDate;
     }
-    mapping (address => CabalInfo) info;
+    mapping (address => Info) infoMap;
 
     Cabal[] public allCabals;
     Proposal[] public allProposals;
 
-    function count()
+    function AllCabals()
+    public
+    {
+        infoMap[msg.sender].membership = Membership.BOARD;
+    }
+
+    event NewVoter(address voter);
+    event Deregistered(address voter);
+    event NewBoard(address board);
+    event NewProposal(Proposal proposal);
+    event NewCabal(Cabal cabal);
+
+    function cabalCount()
     public view
     returns (uint256) {
         return allCabals.length;
     }
 
-    function registerCabal(Cabal _cabal, bytes _data)
+    // To register a Cabal, you must
+    // - implement CabalInterface
+    // - open-source your Cabal
+    function registerCabal(Cabal _cabal)
     external {
-        require(info[_cabal].membership == Membership.UNCONTACTED);
-        CabalInfo storage cInfo = info[_cabal];
-        cInfo.membership = Membership.PENDING;
-        cInfo.data = _data;
+        Info storage info = infoMap[_cabal];
+        require(info.membership == Membership.UNCONTACTED);
+        info.membership = Membership.PENDING_CABAL;
+        NewCabal(_cabal);
     }
+
+    function confirmCabal(Cabal _cabal)
+    external {
+        Info storage info = infoMap[_cabal];
+        require(info.membership == Membership.PENDING_CABAL);
+        // FIXME require permissions
+        info.membership = Membership.CABAL;
+        allCabals.push(_cabal);
+    }
+
+    function register()
+    external payable
+    {
+        assert(msg.value == registrationBounty);
+        Info storage info = infoMap[msg.sender];
+        require(info.membership == Membership.UNCONTACTED);
+        info.membership = Membership.VOTER;
+        info.registrationDate = now;
+        NewVoter(msg.sender);
+    }
+
+    function deregister()
+    external
+    {
+        Info storage info = infoMap[msg.sender];
+        assert(info.membership == Membership.VOTER);
+        assert(info.registrationDate > now - 7 days);
+        info.membership = Membership.UNCONTACTED;
+        msg.sender.transfer(registrationBounty);
+        Deregistered(msg.sender);
+    }
+
+    function canVote(address _voter) public view
+    returns (bool)
+    {
+        Info storage info = infoMap[_voter];
+        return info.membership == Membership.VOTER
+            || info.membership == Membership.BOARD;
+    }
+
+    function isProposal(address _proposal) public view
+    returns (bool)
+    {
+        return infoMap[_proposal].membership == Membership.PROPOSAL;
+    }
+
+    // board members gain superpowers but cannot claim their deposit
+    function appoint(address _board)
+    external
+    {
+        assert(infoMap[msg.sender].membership == Membership.BOARD);
+        assert(infoMap[_board].membership == Membership.VOTER || infoMap[_board].membership == Membership.UNCONTACTED);
+        infoMap[_board].membership = Membership.BOARD;
+        NewBoard(_board);
+    }
+
+    function propose(bytes _resolution)
+    external
+    returns (Proposal)
+    {
+        Proposal proposal = new Proposal(_resolution, msg.sender);
+        infoMap[proposal].membership = Membership.PROPOSAL;
+        allProposals.push(proposal);
+        NewProposal(proposal);
+        return proposal;
+    }
+
+    // To submit an outside proposal contract, you must:
+    // - ensure it conforms to ProposalInterface
+    // - ensure it properly transfers the VOTE token
+    // - open-source it using Etherscan or equivalent
+    // - pay a manual verification fee
+    function propose(Proposal _proposal)
+    external payable
+    {
+        assert(msg.value == outsideProposalVerificationFee);
+        Info storage info = infoMap[_proposal];
+        assert(info.membership == Membership.UNCONTACTED);
+        info.membership = Membership.PENDING_PROPOSAL;
+    }
+
+    function confirmProposal(Proposal _proposal)
+    external
+    {
+        assert(infoMap[msg.sender].membership == Membership.BOARD);
+        Info storage info = infoMap[_proposal];
+        assert(info.membership == Membership.PENDING_PROPOSAL);
+        info.membership = Membership.PROPOSAL;
+        msg.sender.transfer(outsideProposalVerificationFee);
+        allProposals.push(_proposal);
+        NewProposal(_proposal);
+    }
+
 }
