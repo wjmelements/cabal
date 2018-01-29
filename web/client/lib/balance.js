1423ac62b69ca40e7132140bf0860b6e113c5f40
William Morriss <wjmelements@gmail.com>
2018-01-29 14:49:48 -0800

    don't show account finney

--- web/client/lib/balance.js
+++ web/client/lib/balance.js
@@ -3,20 +3,9 @@ Template.balance.onCreated(function() {
         Balance.set(balance.c[0] / 10);
         Balance.onChange();
     }.bind(this));
-    this.finney = new ReactiveVar();
-    Accounts.current(function(account) {
-        web3.eth.getBalance(account, function (error, balance) {
-            var finney = balance.c[0] / 10 + balance.c[1] / 10e14;
-            finney = Math.floor(finney * 1000) / 1000;
-            this.finney.set(finney);
-        }.bind(this));
-    }.bind(this));
 });
 Template.balance.helpers({
     balance() {
         return Balance.get();
-    },
-    finney() {
-        return Template.instance().finney.get();
     },
 });
