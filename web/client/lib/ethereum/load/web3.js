e2d2ce604968e18ee2eeadee00fe5982cec7a521
William Morriss <wjmelements@gmail.com>
2018-02-07 18:24:52 -0800

    kinda load faster

--- web/client/lib/ethereum/load/web3.js
+++ web/client/lib/ethereum/load/web3.js
@@ -1,14 +1,4 @@
 var loaded = false;
-window.addEventListener('load', function() {
-    if (typeof web3 === 'undefined') {
-        console.log("Using infura");
-        web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/x6jRpmEj17uLQR1TuV1E"));
-    } else if (typeof web3.eth === 'undefined') {
-        web3 = new Web3(web3.currentProvider);
-    }
-    loaded = true;
-    onWeb3();
-});
 var web3Delayed = [];
 function onWeb3() {
     while (web3Delayed.length) {
@@ -24,3 +14,11 @@ Web3Loader = {
         web3Delayed.push(web3Fn);
     },
 }
+if (typeof web3 === 'undefined') {
+    console.log("Using infura");
+    web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/x6jRpmEj17uLQR1TuV1E"));
+} else if (typeof web3.eth === 'undefined') {
+    web3 = new Web3(web3.currentProvider);
+}
+loaded = true;
+onWeb3();
