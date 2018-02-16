c6471ca96dcd69f902932258c369e76019e12115
William Morriss <wjmelements@gmail.com>
2018-02-16 15:27:59 -0800

    use DSWarp to test passage of time

--- contracts/src/allproposals.t.sol
+++ contracts/src/allproposals.t.sol
@@ -1,22 +1,29 @@
 pragma solidity^0.4.18;
 
 import "ds-test/test.sol";
+import "ds-warp/warp.sol";
 
-import "allproposals.sol";
+import "src/allproposals.sol";
 
 contract TokenTest is DSTest {
     Vote token;
     AccountRegistry accountRegistry;
+    DSWarp warp;
     function setUp() public {
         token = new Vote();
         accountRegistry = new AccountRegistry();
         token.migrateAccountRegistry(accountRegistry);
+        warp = new DSWarp();
     }
     function test_transfer() public {
-        accountRegistry.register.value(1 finney)();
+        accountRegistry.register.value(1 finney)(warp);
         assertEq(token.balanceOf(this), 0);
-        assertEq(token.availableFaucet(this), 40);
-        token.faucet();
+        assertEq(token.availableFaucet(this, warp), 0);
+        warp.warp(1 days);
+        assertEq(token.availableFaucet(this, warp), 20);
+        warp.warp(1 days);
+        assertEq(token.availableFaucet(this, warp), 40);
+        token.faucet(warp);
         assertEq(token.balanceOf(this), 40);
         token.transfer(token, 15);
         assertEq(token.balanceOf(this), 25);
