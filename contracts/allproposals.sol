80abf138bf4802adb3096883385f75e82bcb22c9
William Morriss <wjmelements@gmail.com>
2018-02-19 18:44:31 -0800

    space

--- contracts/allproposals.sol
+++ contracts/allproposals.sol
@@ -231,7 +231,7 @@ contract ProxyProposal {
         assembly {
             calldatacopy(0, 0, calldatasize)
             let _retVal := delegatecall(sub(gas,740), 0x0000005E1CBE78009143B44D717423cb01a002B7, 0, calldatasize, 0, 32)
-            switch _retVal case 0 { revert(0,returndatasize) } default { return(0, returndatasize) }
+            switch _retVal case 0 { revert(0, returndatasize) } default { return(0, returndatasize) }
         }
     }
 }
