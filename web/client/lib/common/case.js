900175fb03a8cad63810234b16a2189a9608bb73
William Morriss <wjmelements@gmail.com>
2018-02-26 18:59:34 -0800

    reword

--- web/client/lib/common/case.js
+++ web/client/lib/common/case.js
@@ -4,10 +4,10 @@ Case = {
         if (['.','!'].some(function(k) {
             return str.indexOf(k) >= 0
         })) {
-            warnings.push('Most punctuation is discouraged for brevity\nFor example, use "Mr Bond" instead of "Mr. Bond"');
+            warnings.push('Most punctuation is discouraged for brevity\n"Mr Bond" instead of "Mr. Bond"');
         }
-        if (str.endsWith('?')) {
-            warnings.push('Prefer declarative assertions\nFor example, use "Meat is murder" instead of "Is meat murder?"');
+        if (str.indexOf('?')!=-1) {
+            warnings.push('Prefer declarative assertions\n"Meat is murder" instead of "Is meat murder?"');
         }
         if (str.trim().length != str.length) {
             warnings.push('Trailing or leading whitespace detected');
